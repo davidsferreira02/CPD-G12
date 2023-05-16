88c1cca0a052ff0164868cc4062c98534f214547
David Ferreira <davidferreira@MacBook-Air-de-David-2.local>
2023-05-16 15:37:28 +0100

    o

--- assign2/src/Client.java
+++ assign2/src/Client.java
@@ -1,4 +1,3 @@
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -17,41 +16,21 @@ public class Client {
         BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
         String userInput;
 
-        // Read messages from the server in a separate thread
-        Thread readThread = new Thread(() -> {
-            try {
-                while (true) {
-                    String message = in.readLine();
-                    if (message == null) {
-                        break;
-                    }
-                    System.out.println(message);
-                }
-            } catch (IOException e) {
-                System.err.println("Error reading from server: " + e);
-            }
-        });
-        readThread.start();
-
-        boolean quite = true;
+        boolean quit = false;
         // Send messages to the server
-        // Send messages to the server
-        while (quite) {
+        while (!quit) {
             System.out.print("Enter a message to send to the server (or 'quit' to exit): ");
             userInput = stdIn.readLine();
             out.println(userInput);
             if ("quit".equals(userInput)) {
-                quite = false;
+                quit = true;
             }
         }
 
-// Close the socket
-        readThread.interrupt();
-        try {
-            readThread.join();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+        // Close the resources
+        in.close();
+        out.close();
+        stdIn.close();
         socket.close();
     }
-    }
+}
